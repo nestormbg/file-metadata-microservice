264e0f178b2cfa4536e1598702ab562dab755af9
Néstor Benitez <nestormbg1011@gmail.com>
2023-11-25 08:31:22 -0300

    upload file and manage with multer middlewares

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const upload = multer();
 
 var app = express();
 
@@ -11,7 +13,9 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
 
+});
 
 
 const port = process.env.PORT || 3000;
